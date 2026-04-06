576d079f84599cee18738219801108eb6b4291bc
brunoosribeiro2007-dot <brunoosribeiro2007@gmail.com>
2026-04-06 14:20:18 -0300

    fix(logger): enable console logging in production for Render visibility

--- src/config/logger.js
+++ src/config/logger.js
@@ -13,13 +13,12 @@ const logger = winston.createLogger({
   ],
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
+// Adiciona o console por padrão para vermos nos logs do Render/Cloud
+logger.add(new winston.transports.Console({
     format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.simple()
+        winston.format.colorize(),
+        winston.format.simple()
     ),
-  }));
-}
+}));
 
 module.exports = logger;
